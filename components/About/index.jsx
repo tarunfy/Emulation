8a9a572c5d998dbdad2aaabe5350b7d429c07a01
Tarun <tarun@Taruns-MacBook-Pro.local>
2022-10-25 01:18:32 +0530

    fixed about us section

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -6,14 +6,17 @@ const About = () => {
         Collection of <code className="tracking-tighter">tech</code> enthusiasts
         👨‍💻
       </h1>
-      <div className="p-10 bg-gradient-to-t from-[#0b1121] via-[#1c2d5b]  to-[#0070F3] shadow-custom shadow-white/10 w-fit mx-auto mt-6 rounded-xl">
-        <p className="text-center tracking-wide text-lg text-white max-w-2xl mx-auto mt-4">
-          This club is running by the students of the Accurate group of
-          Institutions! We currently working on Web development , Android
-          Development , Competetive Programming.. We are currently learning on
-          cross platform. We looking to collaborate on different hackathon and
-          coding competition. We looking to help others.
-        </p>
+      <div className="relative max-w-2xl mx-auto">
+        <div className="absolute blur -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600  rounded-lg mx-auto "></div>
+        <div className="relative p-10 bg-[#0b1121] rounded-lg max-w-fit mx-auto mt-6">
+          <p className="text-center tracking-wide text-lg text-white max-w-2xl mx-auto mt-4">
+            This club is running by the students of the Accurate group of
+            Institutions! We currently working on Web development , Android
+            Development , Competetive Programming.. We are currently learning on
+            cross platform. We looking to collaborate on different hackathon and
+            coding competition. We looking to help others.
+          </p>
+        </div>
       </div>
     </div>
   );
