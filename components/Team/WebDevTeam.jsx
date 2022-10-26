ed3842d0432fd0775097fb37bc6146be5495719f
Tarun <tarun@Taruns-MacBook-Pro.local>
2022-10-26 19:35:23 +0530

    fixed teams responsivessness

--- components/Team/WebDevTeam.jsx
+++ components/Team/WebDevTeam.jsx
@@ -4,7 +4,7 @@ import Card from "../Card";
 const WebDevTeam = () => {
   return (
     <div className="mt-10">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid  sm:grid-cols-3 grid-cols-2 gap-4">
         {data.web.map((member, index) => (
           <Card key={index} info={member} />
         ))}
--- components/Team/index.jsx
+++ components/Team/index.jsx
@@ -8,47 +8,43 @@ const Team = () => {
   const [team, setTeam] = useState("core");
   return (
     <div className="relative after:inset-0 after:z-0  after:bg-center after:bg-no-repeat after:bg-team-bg after:absolute py-32">
-      <div className="relative z-20 max-w-5xl mx-auto space-y-10">
+      <div className="relative z-20 max-w-5xl px-10 xl:px-0 mx-auto space-y-10">
         <h1 className="text-white text-center text-6xl font-bold tracking-tight">
           “Meet the team of Emulation”
         </h1>
 
-        <ul className="flex w-full items-center justify-between space-x-4 text-white/90 font-normal text-lg">
+        <ul className="grid w-full sm:grid-cols-4 grid-cols-2 sm:gap-4 gap-2 text-center text-white/90 font-normal  sm:text-sm md:text-base lg:text-xl text-2xl flex-wrap">
           <li
             className={`${
               team === "core" &&
-              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1] text-white px-4 py-2 rounded-md font-medium"
-            }`}
+              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1] text-white  rounded-md font-medium"
+            } px-4 py-2`}
           >
             <button onClick={() => setTeam("core")}>Core</button>
           </li>
           <li
             className={`${
               team === "web" &&
-              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1] text-white font-medium px-4 py-2 rounded-md"
-            } `}
+              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1] text-white font-medium rounded-md"
+            } px-4 py-2 `}
           >
-            <button onClick={() => setTeam("web")}>Web Developement</button>
+            <button onClick={() => setTeam("web")}>Web Dev</button>
           </li>
           <li
             className={`${
               team === "android" &&
-              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1] text-white px-4 py-2 font-medium rounded-md"
-            }`}
+              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1] text-white font-medium rounded-md"
+            }  px-4 py-2`}
           >
-            <button onClick={() => setTeam("android")}>
-              Android Development
-            </button>
+            <button onClick={() => setTeam("android")}>Android Dev</button>
           </li>
           <li
             className={`${
               team === "competitive" &&
-              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1] text-white px-4 py-2 rounded-md font-medium"
-            }`}
+              "bg-gradient-to-r from-[#3b31b0] to-[#5c50e1]  text-white rounded-md font-medium"
+            }  px-4 py-2`}
           >
-            <button onClick={() => setTeam("competitive")}>
-              Competitive Programming
-            </button>
+            <button onClick={() => setTeam("competitive")}>Competitive</button>
           </li>
         </ul>
 
